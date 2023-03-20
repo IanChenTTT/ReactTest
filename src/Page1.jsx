8716c4871384c4899920d782dbae8380d9729021
Ian Chen <ianchenhappy438@gmail.com>
2023-03-20 15:16:34 +0800

    Finish First layering+Animation  On branch master  Changes to be committed: 	modified:   src/Page1.jsx     modified:   src/style.css

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,15 +1,30 @@
-export default function Page1(){
-   return(
+export default function Page1() {
+  return (
     <div id="Page1">
-        {/* <div className="CircleLayer"></div> */}
-        <span className="CircleLayer"></span>
+      <div id="Circlewrapper">
+        <span className="CircleLayer">
+          <h1>Portfolio
+            <br />
+            I like building web
+          </h1>
+          </span>
+          <span className="OuterLayer"></span>
+      </div>
     </div>
-   ); 
+  );
 }
-function CircleLayer({size,layer}){
-   return(
-      <div className="CircleLayer" style={{
-         zIndex: layer,
-      }}></div>
-   )
+function CircleLayer({ size, layer }) {
+  return (
+    <div
+      style={{
+        zIndex: layer,
+        border: "16px solid",
+        borderRadius: "50%",
+        borderTop: " 16px solid #3498db",
+        height: "30vh",
+        width: "15vh",
+        gridArea: "three",
+      }}
+    ></div>
+  );
 }
