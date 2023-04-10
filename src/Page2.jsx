9cf55082ec79768f1b33eb211836f42ee0ab590f
Ian Chen <ianchenhappy438@gmail.com>
2023-04-10 12:44:49 +0800

    PageOk

---
+++ src/Page2.jsx
@@ -0,0 +1,22 @@
+export default function Page2() {
+  return (
+    <>
+      <div id="Page2" className="EachPage">
+        <div
+          id="Circlewrapper"
+          style={{
+            gridArea: "two",
+          }}
+        >
+          <span className="CircleLayer">
+            <h3>
+                <span>
+                    Skill
+                </span>
+            </h3>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
