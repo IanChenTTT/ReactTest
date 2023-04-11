ca7a29fc24f80050cd4abc2ac2cc1f59b5eacd4d
Chen <ianchenhappy438@gmail.com>
2023-04-11 22:29:28 +0800

    Page2_90%

--- src/page2.jsx
+++ src/page2.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 export default function Page2() {
   return (
-   <>
+    <>
+      <Page2Rfront></Page2Rfront>
+    </>
+  );
+}
+function Page2Rfront(props) {
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    setIsActive((current) => !current);
+  };
+  return (
+    <>
       <div id="Page2" className="Eachpage">
         <div id="Circlewrapper">
           <span className="CircleLayer " id="P2_Circle1">
@@ -19,26 +32,78 @@ export default function Page2() {
               </span>
             </h3>
           </span>
-
-          <button id="Myskill"></button>
         </div>
         <div id="Circlewrapper">
           <span className="CircleLayer" id="P2_Circle3">
             <p className="AwsomeFont">
               <br />
-              專長
-              <br />
-              前端:
-              <br />
-              從html/css 到 JS 在 JS/ES7 到 react JS
-              <br />
-              伺服器:
-              <br />
-              Node js/php 資料庫 Apache;
+              {!isActive ? <P2_1CirIsFront></P2_1CirIsFront> :
+               <P2_2CirIsBack></P2_2CirIsBack>};
             </p>
           </span>
+        </div>
+        <div
+          id="Circlewrapper"
+          style={{
+            zIndex: isActive ? `10` : `-10`,
+          }}
+          onClick={handleClick}
+        >
+          <div className="CircleLayer AwsomeFont">
+               {isActive? <P2_2CirIsFront></P2_2CirIsFront>
+           : <P2_2CirIsBack></P2_2CirIsBack>};
+          </div>
         </div>
       </div>
     </>
   );
+}
+
+
+function P2_1CirIsFront() {
+  return (
+    <>
+    <span style={{ fontWeight: 600 }}>專長</span>
+      <br />
+      前端:
+      <br />
+      從html/css 到 JS
+      <br />
+      JS/ES7 到 react JS
+      <br />
+      伺服器:
+      <br />
+      NodeJS+Express/php+Apache
+      <br />
+      資料庫
+      <br />
+      Mysql; 
+    </>
+  );
+}
+function P2_2CirIsBack() {
+  return (
+    <>
+      P
+      <br />
+      R
+      <br />
+      E
+      <br />
+      S
+      <br />S
+    </>
+  );
+}
+function P2_2CirIsFront(){
+return(
+  <>
+  <div  id="P2_4Cir">
+    <div style={{gridArea: 1}}>
+
+    </div>
+
+  </div>
+  </>
+)
 }
