ae9c24d07f0cfdb001db7e4cecae0012ec30cd82
Chen <ianchenhappy438@gmail.com>
2023-04-11 14:03:48 +0800

    aa

--- src/page2.jsx
+++ src/page2.jsx
@@ -1,41 +1,19 @@
 export default function Page2() {
   return (
-       <>
-      <div id="Page2" className="Eachpage">
-        <div id="Circlewrapper">
-          <span className="CircleLayer " id="P2_Circle1">
+    <>
+      <div id="Page2" className="EachPage">
+        <div
+          id="Circlewrapper"
+          style={{
+            gridArea: "two",
+          }}
+        >
+          <span className="CircleLayer">
             <h3>
-              <span>About ME</span>
+                <span>
+                    Skill
+                </span>
             </h3>
-          </span>
-        </div>
-        <div id="Circlewrapper">
-          <span className="CircleLayer " id="P2_Circle2">
-            <h3>
-              <span>
-                元智資傳系
-                <br />
-                陳衍易
-              </span>
-            </h3>
-          </span>
-
-          <button id="Myskill"></button>
-        </div>
-        <div id="Circlewrapper">
-          <span className="CircleLayer" id="P2_Circle3">
-            <p className="AwsomeFont">
-              <br />
-              專長
-              <br />
-              前端:
-              <br />
-              從html/css 到 JS 在 JS/ES7 到 react JS
-              <br />
-              伺服器:
-              <br />
-              Node js/php 資料庫 Apache;
-            </p>
           </span>
         </div>
       </div>
